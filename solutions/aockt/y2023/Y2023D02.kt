f92091c34ce239c87f68197a7e868dba10f0304a
Alex Schiff <alex.schiff+github@gmail.com>
2023-12-02 15:44:59 +0000

    First attempt

--- solutions/aockt/y2023/Y2023D02.kt
+++ solutions/aockt/y2023/Y2023D02.kt
@@ -1,8 +1,28 @@
 package aockt.y2023
 
 import io.github.jadarma.aockt.core.Solution
+import aockt.y2023.Match
 
 object Y2023D02 : Solution {
-    private val MATCH = Match(12, 13, 14)
-    override fun partOne(input: String) = 0
-}
+    private const val MAIN_REGEX_STRING =
+        """^(?<gamePrefix>Game\s)(?<gameId>[1-9]|[1-9]\d|100):\s(?<matches>.*)$"""
+        private const val RED_REGEX_STRING = """^(?:.*)(?<count>[1-9]|[1-9]\d)(?<color>\sred)?(?:.*)$"""
+        private const val GREEN_REGEX_STRING = """^(?:.*)(?<count>[1-9]|[1-9]\d)(?<color>\sgreen)?(?:.*)$"""
+        private const val BLUE_REGEX_STRING = """^(?:.*)(?<count>[1-9]|[1-9]\d)(?<color>\sblue)?(?:.*)$"""
+
+    private fun parseMatchesString(matchesString: String) = matchesString.split(";").map{
+            val redMatch = RED_REGEX_STRING.toRegex().find(it)!!
+            val greenMatch = GREEN_REGEX_STRING.toRegex().find(it)!!
+            val blueMatch = BLUE_REGEX_STRING.toRegex().find(it)!!
+            return Match(
+                if(redMatch.groups["color"]!!.value.isBlank()) 0 else redMatch.groups["count"]!!.value.toInt(),
+                if(greenMatch.groups["color"]!!.value.isBlank()) 0 else greenMatch.groups["count"]!!.value.toInt(),
+                if(blueMatch.groups["color"]!!.value.isBlank()) 0 else blueMatch.groups["count"]!!.value.toInt()
+                )
+        }.filter{it.isValid()}
+    
+
+    override fun partOne(input: String) = input.lines().map { MAIN_REGEX_STRING.toRegex().find(it)!! }.map{
+        it.groups["gameId"]!!.value.toInt() to parseMatchesString(it.groups["matches"]!!.value)}
+        
+    }
