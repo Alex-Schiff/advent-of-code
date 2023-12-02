9bf13d9daa3757bd5100c80db2ffc93914d834c5
Alex Schiff <alex.schiff+github@gmail.com>
2023-12-02 13:24:09 +0000

    Refactoring

--- solutions/aockt/y2023/Y2023D01.kt
+++ solutions/aockt/y2023/Y2023D01.kt
@@ -4,17 +4,17 @@ import io.github.jadarma.aockt.core.Solution
 
 object Y2023D01 : Solution {
     private const val DIGIT = """one|two|three|four|five|six|seven|eight|nine|\d"""
-    private val DIGITS_MAP: Map<String, Int> =
+    private val DIGITS_MAP: Map<String, String> =
         mapOf(
-            "one" to 1,
-            "two" to 2,
-            "three" to 3,
-            "four" to 4,
-            "five" to 5,
-            "six" to 6,
-            "seven" to 7,
-            "eight" to 8,
-            "nine" to 9
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9"
         )
 
     override fun partOne(input: String) =
@@ -46,20 +46,8 @@ object Y2023D01 : Solution {
                     it.groups["secondDigit"]!!.value
                 )
             }
-            .map {
-                Pair(
-                    try {
-                        Digits.valueOf(it.first).digitValue
-                    } catch (e: IllegalArgumentException) {
-                        it.first.toInt()
-                    },
-                    try {
-                        Digits.valueOf(it.second).digitValue
-                    } catch (e: IllegalArgumentException) {
-                        it.second.toInt()
-                    }
-                )
+            .map { it ->
+                "${DIGITS_MAP.getOrDefault(it.first, it.first)}${DIGITS_MAP.getOrDefault(it.second, it.second}".toInt()
             }
-            .map { "${it.first}${it.second}".toInt() }
             .sum()
 }
