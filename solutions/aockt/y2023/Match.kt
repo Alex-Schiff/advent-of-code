1f3889ef59df1f255308103d2662a17d29053aad
Alex Schiff <alex.schiff@gmail.com>
2023-12-02 09:01:09 -0500

    Init

---
+++ solutions/aockt/y2023/Match.kt
@@ -0,0 +1,4 @@
+package aockt.y2023
+
+/** Match is a class that holds information about how many balls were withdrawn from the bag. */
+data class Match(val redCount: Int, val greenCount: Int, val blueCount: Int)
---
+++ solutions/aockt/y2023/Y2023D02.kt
@@ -0,0 +1,8 @@
+package aockt.y2023
+
+import io.github.jadarma.aockt.core.Solution
+
+object Y2023D02 : Solution {
+    private val MATCH = Match(12, 13, 14)
+    override fun partOne(input: String) = 0
+}
