0475c8bc79dc4a2d39cbb9c6f55c849ffae7468b
Alex Schiff <alex.schiff+github@gmail.com>
2023-12-03 15:37:51 +0000

    Part 2

--- solutions/aockt/y2023/Grid.kt
+++ solutions/aockt/y2023/Grid.kt
@@ -1,7 +1,9 @@
 package aockt.y2023
 
 typealias Grid = List<List<Char>>
+
 typealias Point = Pair<Int, Int>
+
 fun Grid.get(coordinates: Point?): Char? {
     return if (coordinates == null) {
         null
@@ -10,39 +12,148 @@ fun Grid.get(coordinates: Point?): Char? {
     }
 }
 
-fun List<String>.toGrid(): Grid = this.map { line -> line.toCharArray().toList() }
+fun List<String>.toGrid(): Grid = this.map { it.toCharArray().toList() }
 
-fun Grid.getAdjacents(coords: Point): List<Char> {
+fun String.toGrid(): Grid = this.lines().toGrid()
+
+fun Grid.getAdjacents(coordinates: Point): List<Char> {
     val ret = mutableListOf<Char>()
-    if(coords.first > 0) {
-        if(coords.second > 0) {
-            ret.add(this[coords.first-1][coords.second-1])
+    if (coordinates.first > 0) {
+        if (coordinates.second > 0) {
+            ret.add(this[coordinates.first - 1][coordinates.second - 1])
         }
-        ret.add(this[coords.first-1][coords.second])
-        if(coords.second < (this.first().size -1)) {
-            ret.add(this[coords.first-1][coords.second+1])
+        ret.add(this[coordinates.first - 1][coordinates.second])
+        if (coordinates.second < (this.first().size - 1)) {
+            ret.add(this[coordinates.first - 1][coordinates.second + 1])
         }
     }
 
-    if(coords.second > 0) {
-        ret.add(this[coords.first][coords.second-1])
+    if (coordinates.second > 0) {
+        ret.add(this[coordinates.first][coordinates.second - 1])
     }
 
-    if(coords.second < (this.first().size -1)) {
-        ret.add(this[coords.first][coords.second+1])
+    if (coordinates.second < (this.first().size - 1)) {
+        ret.add(this[coordinates.first][coordinates.second + 1])
     }
 
-    if(coords.first < (this.size -1)) {
-        if(coords.second > 0) {
-            ret.add(this[coords.first+1][coords.second-1])
+    if (coordinates.first < (this.size - 1)) {
+        if (coordinates.second > 0) {
+            ret.add(this[coordinates.first + 1][coordinates.second - 1])
         }
-        ret.add(this[coords.first+1][coords.second])
-        if(coords.second < (this.first().size -1)) {
-            ret.add(this[coords.first+1][coords.second+1])
+        ret.add(this[coordinates.first + 1][coordinates.second])
+        if (coordinates.second < (this.first().size - 1)) {
+            ret.add(this[coordinates.first + 1][coordinates.second + 1])
         }
     }
 
     return ret.toList()
 }
 
-fun Grid.isValidDigit(coords: Point): Boolean = getAdjacents(coords).any { !it.isDigit() && it != '.'}
+fun Grid.isValidDigit(coordinates: Point): Boolean =
+    getAdjacents(coordinates).any { !it.isDigit() && it != '.' }
+
+public fun String.extractValidNumbersSum(grid: Grid, lineIndex: Int): Int {
+    var sum = 0
+
+    var currentNumber = ""
+    var index = 0
+    var isValid = false
+    while (index < this.length) {
+        if (!this[index].isDigit()) {
+            if (currentNumber.isNotEmpty() && isValid) sum += currentNumber.toInt()
+            currentNumber = ""
+            isValid = false
+        } else {
+            currentNumber += this[index]
+            isValid = isValid || grid.isValidDigit(lineIndex to index)
+        }
+        index++
+    }
+
+    if (currentNumber.isNotEmpty() && isValid) sum += currentNumber.toInt()
+    return sum
+}
+
+fun Grid.extractAllStarPositions(): List<Point> = flatMapIndexed { rowIndex, row ->
+    row.mapIndexedNotNull { colIndex, value ->
+        if (value == '*') {
+            rowIndex to colIndex
+        } else {
+            null
+        }
+    }
+}
+
+fun Grid.extractAllAdjacentNumbers(point: Point): List<Int> {
+    val sameLineLeft = extractAdjacentSameLineLeft(point)
+    val sameLineRight = extractAdjacentSameLineRight(point)
+    val prevLine = extractAdjacentPrevLine(point)
+    val nextLine = extractAdjacentNextLine(point)
+    return listOfNotNull(sameLineLeft?.toInt(), sameLineRight?.toInt()) + prevLine + nextLine
+}
+
+fun Grid.extractAdjacentSameLineLeft(point: Point): String? {
+    val line = this[point.first]
+    var index = point.second - 1
+    var ret = ""
+    while (index >= 0) {
+        if (line[index].isDigit()) {
+            ret += line[index]
+        } else {
+            break
+        }
+        index--
+    }
+    return if (ret.isNotEmpty()) {
+        ret.reversed()
+    } else {
+        null
+    }
+}
+
+fun Grid.extractAdjacentSameLineRight(point: Point): String? {
+    val line = this[point.first]
+    var index = point.second + 1
+    var ret = ""
+    while (index > 0 && index < line.size) {
+        if (line[index].isDigit()) {
+            ret += line[index]
+        } else {
+            break
+        }
+        index++
+    }
+    return ret.ifEmpty { null }
+}
+
+fun Grid.extractAdjacentPrevLine(point: Point): List<Int> {
+    if (point.first <= 0) {
+        return listOf()
+    }
+
+    val upper = point.first - 1 to point.second
+    return extractAdjacentToPoint(upper)
+}
+
+fun Grid.extractAdjacentNextLine(point: Point): List<Int> {
+    if (point.first >= this.size - 1) {
+        return listOf()
+    }
+
+    val lower = point.first + 1 to point.second
+    return extractAdjacentToPoint(lower)
+}
+
+fun Grid.extractAdjacentToPoint(point: Point): List<Int> {
+    return if (!get(point)!!.isDigit()) {
+        listOfNotNull(
+            extractAdjacentSameLineLeft(point)?.toInt(),
+            extractAdjacentSameLineRight(point)?.toInt(),
+        )
+    } else {
+        val left = (extractAdjacentSameLineLeft(point) ?: "").toString()
+        val right = (extractAdjacentSameLineRight(point) ?: "").toString()
+        val ret = "$left${get(point)!!}$right".toInt()
+        listOf(ret)
+    }
+}
--- solutions/aockt/y2023/Y2023D03.kt
+++ solutions/aockt/y2023/Y2023D03.kt
@@ -4,7 +4,20 @@ import io.github.jadarma.aockt.core.Solution
 
 object Y2023D03 : Solution {
     override fun partOne(input: String): Int {
-        val numberRegex = """\d+""".toRegex()
-        return 0
+        val grid = input.toGrid()
+        return input
+            .lines()
+            .mapIndexed { lineIndex, line -> line.extractValidNumbersSum(grid, lineIndex) }
+            .sum()
+    }
+
+    override fun partTwo(input: String): Int {
+        val grid = input.toGrid()
+        return grid
+            .extractAllStarPositions()
+            .associateWith { grid.extractAllAdjacentNumbers(it) }
+            .filter { it.value.size == 2 }
+            .map { it.value.fold(1) { total, next -> total * next } }
+            .sum()
     }
 }
