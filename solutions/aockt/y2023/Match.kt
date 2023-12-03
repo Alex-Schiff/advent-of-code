0807abdda868aae9cb7b9956dbdd19ee20146bf3
Alex Schiff <alex.schiff+github@gmail.com>
2023-12-03 14:21:48 +0000

    Day 2 solution

--- solutions/aockt/y2023/Match.kt
+++
@@ -1,9 +0,0 @@
-package aockt.y2023
-
-/** Match is a class that holds information about how many balls were withdrawn from the bag. */
-data class Match(val redCount: Int, val greenCount: Int, val blueCount: Int)
-
-val MATCH = Match(12, 13, 14)
-
-fun Match.isValid(): Boolean =
-    redCount <= MATCH.redCount && greenCount <= MATCH.greenCount && blueCount <= MATCH.blueCount
--- solutions/aockt/y2023/Y2023D02.kt
+++ solutions/aockt/y2023/Y2023D02.kt
@@ -3,39 +3,27 @@ package aockt.y2023
 import io.github.jadarma.aockt.core.Solution
 
 object Y2023D02 : Solution {
-    private const val MAIN_REGEX_STRING =
-        """^\s*(?<gamePrefix>Game)\s(?<gameId>[1-9]|[1-9]\d|100):\s(?<matches>.*)$"""
-    private const val RED_REGEX_STRING = """^.*\s(?<count>[1-9]|[1-9]\d)\s(?<color>red).*$"""
-    private const val GREEN_REGEX_STRING = """^.*\s(?<count>[1-9]|[1-9]\d)\s(?<color>green).*$"""
-    private const val BLUE_REGEX_STRING = """^.*\s(?<count>[1-9]|[1-9]\d)\s(?<color>blue).*$"""
-
-    private fun parseMatchesString(matchesString: String): List<Match> =
-        matchesString.split(";").map {
-            val redMatch = RED_REGEX_STRING.toRegex().find(it)
-            val redValue: Int =
-                if (redMatch?.groups?.get("color") == null) 0
-                else redMatch.groups["count"]!!.value.toInt()
-
-            val greenMatch = GREEN_REGEX_STRING.toRegex().find(it)
-            val greenValue: Int =
-                if (greenMatch?.groups?.get("color") == null) 0
-                else greenMatch.groups["count"]!!.value.toInt()
-
-            val blueMatch = BLUE_REGEX_STRING.toRegex().find(it)
-            val blueValue: Int =
-                if (blueMatch?.groups?.get("color") == null) 0
-                else blueMatch.groups["count"]!!.value.toInt()
-            Match(redValue, greenValue, blueValue)
-        }
+    private fun findRequiredDice(line: String, color: String): Int {
+        return Regex("\\d+(?= $color)").findAll(line).toList().maxOfOrNull { it.value.toInt() } ?: 0
+    }
 
     override fun partOne(input: String): Int =
         input
             .lines()
-            .map { MAIN_REGEX_STRING.toRegex().find(it)!! }
-            .map {
-                it.groups["gameId"]!!.value.toInt() to
-                    parseMatchesString(it.groups["matches"]!!.value)
+            .mapNotNull { line ->
+                // Determine which games would have been possible if the bag had been loaded with
+                // only
+                // 12 red cubes, 13 green cubes, and 14 blue cubes.
+                if (
+                    findRequiredDice(line, "red") <= 12 &&
+                        findRequiredDice(line, "green") <= 13 &&
+                        findRequiredDice(line, "blue") <= 14
+                ) {
+                    // game is possible, grab the game # which is the first num in the line
+                    Regex("\\d+").find(line)!!.value.toInt()
+                } else {
+                    null // not possible so use null to drop this game from our mapping
+                }
             }
-            .filter { it.second.all { match -> match.isValid() } }
-            .sumOf { it.first }
+            .sum()
 }
